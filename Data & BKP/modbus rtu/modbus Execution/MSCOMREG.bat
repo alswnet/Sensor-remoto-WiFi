6808fbc416f073afc46a9e6d92d49181c45df963
luiscd7 <info@solucionesempresariales.us>
2015-09-08 10:22:47 -0600

    ingreso de datos de modbusrtu

---
+++ Data & BKP/modbus rtu/modbus Execution/MSCOMREG.bat
@@ -0,0 +1,63 @@
+echo off
+
+regsvr32 /s MSCOMM32.OCX
+reg add "HKEY_CLASSES_ROOT\Licenses" /v "" /t REG_SZ /d "Licensing: Copying the keys may be a violation of established copyrights." /f
+echo // Masked Edit Control 6.0 license key
+reg add "HKEY_CLASSES_ROOT\Licenses\BC96F860-9928-11cf-8AFA-00AA00C00905" /v "" /t REG_SZ /d  "mmimfflflmqmlfffrlnmofhfkgrlmmfmqkqj" /f
+echo // Chart Control 6.0 (OLEDB) license key
+reg add "HKEY_CLASSES_ROOT\Licenses\12B142A4-BD51-11d1-8C08-0000F8754DA1" /v ""  /t REG_SZ /d  "aadhgafabafajhchnbchehfambfbbachmfmb" /f
+echo  // Common Dialog Control 6.0 license key
+reg add "HKEY_CLASSES_ROOT\Licenses\4D553650-6ABE-11cf-8ADB-00AA00C00905" /v ""  /t REG_SZ /d  "gfjmrfkfifkmkfffrlmmgmhmnlulkmfmqkqj" /f
+echo  // ADO Data Control 6.0 (OLEDB) license key
+reg add "HKEY_CLASSES_ROOT\Licenses\C4145310-469C-11d1-B182-00A0C922E820" /v "" /t REG_SZ /d  "konhqhioohihphkouimonhqhvnwiqhhhnjti" /f
+echo // Common Controls-3 6.0 license key
+reg add "HKEY_CLASSES_ROOT\Licenses\38911DA0-E448-11D0-84A3-00DD01104159" /v "" /t REG_SZ /d  "mcpckchcdchjcjcclidcgcgchdqdcjhcojpd" /f
+echo // Windows Common Controls-2 5.0 (SP2) license key
+reg add "HKEY_CLASSES_ROOT\Licenses\9E799BF1-8817-11cf-958F-0020AFC28C3B" /v "" /t REG_SZ /d  "uqpqnqkjujkjjjjqwktjrjkjtkupsjnjtoun" /f
+echo  // Windows Common Controls license key
+reg add "HKEY_CLASSES_ROOT\Licenses\57CBF9E0-6AA7-11cf-8ADB-00AA00C00905" /v "" /t REG_SZ /d  "aahakhchghkhfhaamghhbhbhkbpgfhahlfle" /f
+echo  // Data Bound Grid Control 5.0(SP3) license key
+reg add "HKEY_CLASSES_ROOT\Licenses\556C75F1-EFBC-11CF-B9F3-00A0247033C4" /v ""  /t REG_SZ /d  "xybiedobrqsprbijaegcbislrsiucfjdhisl" /f
+echo // Data Bound List Controls 6.0 license key
+reg add "HKEY_CLASSES_ROOT\Licenses\096EFC40-6ABF-11cf-850C-08002B30345D" /v ""  /t REG_SZ /d  "knsgigmnmngnmnigthmgpninrmumhgkgrlrk" /f
+echo  // Internet Transfer Control 6.0 license key
+reg add "HKEY_CLASSES_ROOT\Licenses\78E1BDD1-9941-11cf-9756-00AA00C00908" /v ""  /t REG_SZ /d  "yjrjvqkjlqqjnqkjvprqsjnjvkuknjpjtoun" /f
+echo  // Multimedia Control 6.0 license key
+reg add "HKEY_CLASSES_ROOT\Licenses\B1EFCCF0-6AC1-11cf-8ADB-00AA00C00905" /v ""  /t REG_SZ /d  "qqkjvqpqmqjjpqjjvpqqkqmqvkypoqjquoun" /f
+echo  // Chart Control 6.0 license key 
+reg add "HKEY_CLASSES_ROOT\Licenses\7C35CA30-D112-11cf-8E72-00A0C90F26F8" /v ""  /t REG_SZ /d  "whmhmhohmhiorhkouimhihihwiwinhlosmsl" /f
+echo  // Windows Common Controls-2 6.0 license key
+reg add "HKEY_CLASSES_ROOT\Licenses\4F86BADF-9F77-11d1-B1B7-0000F8753F5D" /v ""  /t REG_SZ /d  "iplpwpnippopupiivjrioppisjsjlpiiokuj" /f
+echo // Windows Common Controls 6.0 license key
+reg add "HKEY_CLASSES_ROOT\Licenses\ED4B87C4-9F76-11d1-8BF7-0000F8754DA1" /v ""  /t REG_SZ /d  "knlggnmntgggrninthpgmnngrhqhnnjnslsh" /f
+echo  // Comm Control 6.0 license key
+reg add "HKEY_CLASSES_ROOT\Licenses\4250E830-6AC2-11cf-8ADB-00AA00C00905" /v ""  /t REG_SZ /d  "kjljvjjjoquqmjjjvpqqkqmqykypoqjquoun" /f
+echo  // DataGrid Control 6.0 (OLEDB) license key
+reg add "HKEY_CLASSES_ROOT\Licenses\CDE57A55-8B86-11D0-b3C6-00A0C90AEA82" /v ""  /t REG_SZ /d  "ekpkhddkjkekpdjkqemkfkldoeoefkfdjfqe" /f
+echo // DataList Control 6.0 (OLEDB) license key
+reg add "HKEY_CLASSES_ROOT\Licenses\A133F000-CCB0-11d0-A316-00AA00688B10" /v ""  /t REG_SZ /d  "cibbcimbpihbbbbbnhdbeidiocmcbbdbgdoc" /f
+echo  // DBWin license key
+reg add "HKEY_CLASSES_ROOT\Licenses\D015B071-D2ED-11d0-A31A-00AA00688B10" /v ""  /t REG_SZ /d  "gjdcfjpcmjicjcdcoihcechjlioiccechepd" /f
+echo  // MSDBRPT license key
+reg add "HKEY_CLASSES_ROOT\Licenses\9DF1A470-BA8E-11D0-849C-00A0C90DC8A9" /v ""  /t REG_SZ /d  "cchcqjejhcgcqcfjpdfcdjkckiqikchcojpd" /f
+echo  // FlexGrid Control 6.0 license key
+reg add "HKEY_CLASSES_ROOT\Licenses\72E67120-5959-11cf-91F6-C2863C385E30" /v ""  /t REG_SZ /d  "ibcbbbebqbdbciebmcobmbhifcmciibblgmf" /f
+echo  // MAPI Controls 6.0 license key
+reg add "HKEY_CLASSES_ROOT\Licenses\899B3E80-6AC6-11cf-8ADB-00AA00C00905" /v ""  /t REG_SZ /d  "wjsjjjlqmjpjrjjjvpqqkqmqukypoqjquoun" /f
+echo  // MSRDO 2.0 license key
+reg add "HKEY_CLASSES_ROOT\Licenses\B1692F60-23B0-11D0-8E95-00A0C90F26F8" /v ""  /t REG_SZ /d  "mjjjccncgjijrcfjpdfjfcejpdkdkcgjojpd" /f
+echo  // RemoteData Control 6.0 license key
+reg add "HKEY_CLASSES_ROOT\Licenses\43478d75-78e0-11cf-8e78-00a0d100038e" /v ""  /t REG_SZ /d  "imshohohphlmnhimuinmphmmuiminhlmsmsl" /f
+echo  // Windowless Controls 6.0 license key 
+reg add "HKEY_CLASSES_ROOT\Licenses\80E80EF0-DBBE-11D0-BCE2-00A0C90DCA10" /v ""  /t REG_SZ /d  "qijimitpmpnpxplpvjnikpkpqoxjmpkpoivj" /f
+echo  // PictureClip Control 6.0 license key
+reg add "HKEY_CLASSES_ROOT\Licenses\6FB38640-6AC7-11cf-8ADB-00AA00C00905" /v ""  /t REG_SZ /d  "gdjkokgdldikhdddpjkkekgknesjikdkoioh" /f
+echo  // Rich TextBox Control 6.0 license key
+reg add "HKEY_CLASSES_ROOT\Licenses\DC4D7920-6AC8-11cf-8ADB-00AA00C00905" /v ""  /t REG_SZ /d  "iokouhloohrojhhhtnooiokomiwnmohosmsl" /f
+echo  // Sheridan Tab Control license key
+reg add "HKEY_CLASSES_ROOT\Licenses\190B7910-992A-11cf-8AFA-00AA00C00905" /v ""  /t REG_SZ /d  "gclclcejjcmjdcccoikjlcecoioijjcjnhng" /f
+echo  // SysInfo Control 6.0 license key 
+reg add "HKEY_CLASSES_ROOT\Licenses\E32E2733-1BC5-11d0-B8C3-00A0C90DCA10" /v ""  /t REG_SZ /d  "kmhfimlflmmfpffmsgfmhmimngtghmoflhsg" /f
+echo  // Winsock Control 6.0 license key
+reg add "HKEY_CLASSES_ROOT\Licenses\2c49f800-c2dd-11cf-9ad6-0080c7e7b78d" /v ""  /t REG_SZ /d  "mlrljgrlhltlngjlthrligklpkrhllglqlrk" /f
+
---
+++ Data & BKP/modbus rtu/modbus Execution/Readme.txt
@@ -0,0 +1,1 @@
+Open "MSCOMREG.bat"
